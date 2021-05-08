f1988ebf1802289128e60d6c44b2bd8fa31a559e
Catalpam <893816760@qq.com>
2021-05-08 17:46:50 +0800

    just a commit

--- lost_found.go
+++ lost_found.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"lost_found/common"
+	"lost_found/controller"
+	"lost_found/controller/webController"
+	"lost_found/handler"
 )
 
-// for redis store and logrus
-// var conf = configs.ConfigWithLogrusAndRedisStore(constants.DomainFeiShu)
-// var conf = configs.FeishuConfig("https://open.feishu.cn")
-
 func main() {
+	//数据库加载初始化
 	db := common.InitDB()
 	defer db.Close()
+	//更新数据库内容
+	common.ThingDbDefaultInit()
+	common.UpdateStudentList()
 
-	//r := gin.Default()
-	//r = routes.CollectRoute(r)
-	//print("hello")
-	//
-	//panic(r.Run(":1111"))
+	//加载根路由组
+	r := gin.Default()
+	r = CollectRoute(r)
+	r.Run(":1111")
+}
+
+func CollectRoute(r *gin.Engine) *gin.Engine {
+	//️处理飞书服务器传来的事件
+	r.POST("/webhook", handler.EventHandler)
+
+	//小程序路由组
+	//r.POST("/miniLogin", webController.SetLoginStatus)
+	miniRoutes := r.Group("/miniApp")
+	miniRoutes.POST("/userinfo", controller.GetUserInfo)
+
+
+	//后台管理Web登录
+	r.GET("/weblogin", webController.Login302)
+	r.GET("/webcode", webController.SetCookies)
+	//后台管理Web路由组
+	webRoutes := r.Group("/web")
+	//webRoutes.Use(middleware.AuthMiddleware())
+	webRoutes.POST("/userinfo", controller.GetUserInfo)
+	webRoutes.POST("/addfound", controller.AddFound)
+
+
+	return r
 }
